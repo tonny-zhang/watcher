2d6328ee41dd13cd026cd06b42ee7ecf9232d6f4
tonny <wodexintiao@gmail.com>
2013-08-30 15:00:04 +0800

    fixed repeat callback

--- watcherCallback.js
+++ watcherCallback.js
@@ -5,14 +5,21 @@ var watcherUtil = require('./util');
 var Watcher = require('./watcher').Watcher;
 config = watcherUtil.extend(config,require('./config/callback'));//重写watcher
 
+var cache = {};
 var fn = function(d){
-	var filter = config.watcher.forEach(function(v){
-		if(~d.fullname.indexOf(v.path)){
-			if(v.command){
-				watcherUtil.command([v.command,d.fullname].join(' '));
+	var fullname = d.fullname;
+	clearTimeout(cache[fullname]);
+	var tt = setTimeout(function(){
+		delete cache[tt];
+		config.watcher.forEach(function(v){
+			if(~fullname.indexOf(v.path)){
+				if(v.command){
+					watcherUtil.command([v.command,fullname].join(' '));
+				}
 			}
-		}
-	});
+		});
+	},10);
+	cache[fullname] = tt;
 }
 var watcher = new Watcher({ignorePath:/^\..+/})
 .on(Watcher.CREATE_FILE,fn)
