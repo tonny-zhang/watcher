105e91a5a27a7d614ba3b935c6b1aa3118f8517c
tonny <wodexintiao@gmail.com>
2013-09-04 14:52:39 +0800

    add auto mkdir remote

--- memoryWatcher.js
+++ memoryWatcher.js
@@ -15,12 +15,42 @@ var createHttpServer = require('./createHttpServer');
 (function(){
 	var args = process.argv;
 	if(args.length >= 2){
+		//初始化时创建远程文件
+		var createRemoteDirs = function(){
+			var command = config.rsync.user && config.rsync.user != 'root' ? watcherUtil.command.su : watcherUtil.command;
+			var _logInit = watcherUtil.prefixLogSync(config.logPath,'init');
+			var _logError = watcherUtil.errorSync(config.logPath);
+			config.watcher.forEach(function(v){
+				var rsync = v.rsync;
+				if(rsync && !v.isFile){
+					rsync.forEach(function(rv){
+						var port = rv.port;
+						var m = rv.address.match(/(.+?):(.+)/);
+						if(m){
+							var remoteAddress = m[1];
+							var remoteDir = m[2];
+							if(remoteAddress && remoteDir){
+								var _command = ['ssh -p',port,remoteAddress,'"mkdir -p '+remoteDir+'"'].join(' ');
+								command(_command,function(err,data){
+									if(err){
+										_logError(_command,err);
+									}else{
+										_logInit(_command,data);
+									}
+								});
+							}
+						}
+					});
+				}
+			});
+		}
 		var _logWatcher = watcherUtil.prefixLogSync(config.logPath,'watcher');
 		if(args.length == 3 && args[2] == 'reload'){
 			/*重新加载配置*/
 			watcherUtil.curl(config.host,config.port,'/?reload=1',function(err){
 				_logWatcher('reload',err||'Y');
 			});
+			createRemoteDirs();
 			return;
 		}
 		_logWatcher('start');
@@ -60,6 +90,8 @@ var createHttpServer = require('./createHttpServer');
 				}
 				return _path;
 			}
+
+			createRemoteDirs();
 			var watcherCache = config.watcher.info;
 			//进行排序，让父级尽量靠前
 	        Object.keys(watcherCache).sort(function(a,b){
