ff45b83d910374a244d3783173e8d2194cf8be55
tonny <wodexintiao@gmail.com>
2013-10-10 09:41:35 +0800

    add rmTemp.sh

---
+++ shell/rmTemp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+CURRENT_DIR=`dirname $0`
+TEMP_PATH=`/usr/local/bin/node $CURRENT_DIR/tongji/tempPath.js`
+
+find $TEMP_PATH -maxdepth 1 -mtime 1 -exec rm -rf {} \;
---
+++ shell/tongji/tempPath.js
@@ -0,0 +1,2 @@
+var config = require('../../config/index');
+console.log(config.copyToPath);
